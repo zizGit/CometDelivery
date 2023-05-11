b4f20c0e6734a5661dacc96c42ce25f1f8580830
ziz <100286112+zizGit@users.noreply.github.com>
2023-05-11 03:27:28 +0300

    Delete src directory

--- src/Pages/PartnerPage.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useEffect } from "react";
-import PartnerBlock from "../components/PartnerBlock";
-export default function PartnerPage() {
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
-  return <PartnerBlock />;
-}
--- src/redux/store.js
+++
@@ -1,5 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import cart from "./slices/cartSlice";
-export const store = configureStore({
-  reducer: { cart },
-});
